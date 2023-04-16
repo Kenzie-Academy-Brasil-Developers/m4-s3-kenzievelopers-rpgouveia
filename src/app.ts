5fcf2dcb6bb0ead221fd941a9b11bdc9cdabbd3e
Renato Gouveia <rpgouveia@gmail.com>
2023-04-15 22:01:47 -0300

    feat: add retrieveProject

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application } from "express";
 import "dotenv/config";
 import { createDeveloper, createDeveloperInfos, deleteDeveloper, retrieveDeveloper, updateDeveloper } from "./logics/developers.logics";
 import { checkDeveloperId, checkEmailExists, checkInfosExists, checkPreferredOS } from "./middlewares/developers.middlewares";
-import { createProject } from "./logics/projects.logics";
+import { createProject, retrieveProject } from "./logics/projects.logics";
 
 const app: Application = express();
 app.use(express.json());
@@ -14,5 +14,6 @@ app.patch("/developers/:id", checkDeveloperId, checkEmailExists, updateDeveloper
 app.delete("/developers/:id", checkDeveloperId, deleteDeveloper);
 
 app.post("/projects", checkDeveloperId, createProject);
+app.get("/projects/:id", checkDeveloperId, retrieveProject);
 
 export default app;
--- src/logics/projects.logics.ts
+++ src/logics/projects.logics.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { iProject } from "../interfaces/projects.interfaces";
 import format from "pg-format";
-import { QueryResult } from "pg";
+import { QueryConfig, QueryResult } from "pg";
 import { client } from "../database";
 
 const createProject = async (
@@ -27,4 +27,43 @@ const createProject = async (
   return response.status(201).json(queryResult.rows[0]);
 };
 
-export { createProject };
+const retrieveProject = async (
+  request: Request,
+  response: Response
+): Promise<Response> => {
+  const id = request.params.id;
+  const query: string = `
+  SELECT
+    p.id "projectId",
+    p.name "projectName",
+    p.description "projectDescription",
+    p."estimatedTime" "projectEstimatedTime",
+    p.repository "projectRepository",
+    p."startDate" "projectStartDate",
+    p."endDate" "projectEndDate",
+    p."developerId" "projectDeveloperId",
+    pt."technologyId",
+    t."name" "technologyName"
+  FROM
+    projects p
+  LEFT JOIN
+    projects_technologies pt
+  ON
+    p.id = pt."projectId"
+  LEFT JOIN
+    technologies t
+  ON
+    pt."technologyId" = t.id 
+  WHERE
+    p."developerId" = $1;
+`;
+  const queryConfig: QueryConfig = { text: query, values: [id]};
+  const queryResult: QueryResult = await client.query(queryConfig);
+    
+  return response.status(200).json(queryResult.rows);
+};
+
+export {
+  createProject,
+  retrieveProject
+};
